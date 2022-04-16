7b5dc954bf0676366d8a800ceda3f49bf26365b9
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:38 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/resources/empty/EmptyResourceConnection.java
+++ src/main/java/com/semanticcms/resources/empty/EmptyResourceConnection.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-resources-empty - Empty sets of SemanticCMS resources.
- * Copyright (C) 2017  AO Industries, Inc.
+ * Copyright (C) 2017, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-resources-empty.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.resources.empty;
 
 import com.semanticcms.core.resources.ResourceConnection;
