a9c0bc2843ee90fec4d8bf2ffbd474e05048dc8b
AO Industries, Inc <support@aoindustries.com>
2024-10-12 00:24:07 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/semanticcms/resources/empty/EmptyResource.java
+++ src/main/java/com/semanticcms/resources/empty/EmptyResource.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-resources-empty - Empty sets of SemanticCMS resources.
- * Copyright (C) 2017, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2017, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -34,6 +34,9 @@ import java.io.InputStream;
  */
 public class EmptyResource extends Resource {
 
+  /**
+   * Creates a new {@link EmptyResource}.
+   */
   public EmptyResource(EmptyResourceStore store, Path path) {
     super(store, path);
   }
--- src/main/java/com/semanticcms/resources/empty/EmptyResourceConnection.java
+++ src/main/java/com/semanticcms/resources/empty/EmptyResourceConnection.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-resources-empty - Empty sets of SemanticCMS resources.
- * Copyright (C) 2017, 2022  AO Industries, Inc.
+ * Copyright (C) 2017, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -35,6 +35,9 @@ public class EmptyResourceConnection extends ResourceConnection {
 
   private boolean closed;
 
+  /**
+   * Creates a new {@link EmptyResourceConnection}.
+   */
   public EmptyResourceConnection(EmptyResource resource) {
     super(resource);
   }
